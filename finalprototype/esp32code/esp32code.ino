89a26c2de605a9caec52f7f84d6f350c9ab32fb2
Vorapon Kachapornkul <hakao97@gmail.com>
2022-05-22 19:32:20 +0700

    Change timing

--- finalprototype/esp32code/esp32code.ino
+++ finalprototype/esp32code/esp32code.ino
@@ -116,11 +116,6 @@ void loop() {
 }
 
 void Camera_capture() {
-  digitalWrite(Led_Flash, HIGH);
-  delay(100);
-  digitalWrite(Led_Flash, LOW);
-  delay(100);
-  digitalWrite(Led_Flash, HIGH);
   camera_fb_t * fb = NULL;
   delay(200);
   // Take Picture with Camera
@@ -129,7 +124,6 @@ void Camera_capture() {
     Serial.println("Camera capture failed");
     return;
   }
-  digitalWrite(Led_Flash, LOW);
   Send_line(fb->buf, fb->len);
   esp_camera_fb_return(fb);
 
